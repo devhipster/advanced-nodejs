37edbcf5d185a81317d8ba23cf69913a330d039f
HipsterSantos <santoscampos269@gmail.com>
2021-03-24 05:35:28 +0100

    adding user

---
+++ models/users.model.js
@@ -0,0 +1,28 @@
+const { FormatAlignJustifyOutlined } = require('@material-ui/icons');
+var Joi = require('joi');
+const mongoose = require('mongoose')
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,required:true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required:true
+    }
+});
+const User = mongoose.model('User', userSchema);
+
+function validateUser(user){
+    const schema = {
+        email: Joi.string().min(2).required().email(),
+        password: Joi.string().required(),
+        name: Joi.string().min(5).max(50).required()
+    }
+
+    return Joi.validate(user,schema)
+}
+module.exports = User;
